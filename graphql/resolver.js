bd27061f1a3ce21156880594627c5b1e0fe72c76
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-07 10:39:27 +0100

    grapgl user post created

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,9 +1,15 @@
-const User=requre("../models/user")
+const User=require("../models/user")
 
 module.exports={
    createdUser: async({userInput},req)=>{
        const email=userInput.email
        const name=userInput.name
        const password=userInput.password
+
+       const exitingUser=await User.findOne({email:email})
+       if(exitingUser){
+           const error=new Error("user already exits")
+           throw error
+       }
    }
 }
