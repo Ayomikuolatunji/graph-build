0e034cbee416c4d8e62860889dafa181f5767ccc
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-07 10:33:30 +0100

    grapgl user post created

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,7 +1,7 @@
 module.exports={
-    Rootmuation(){
-        return {
-           
-        }
-    }
+   createdUser(arg,req){
+       const email=arg.userInput.email
+       const name=arg.userInput.name
+       const password=arg.userInput.password
+   }
 }
