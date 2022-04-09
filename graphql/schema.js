28279d6526c09796db01bef939fbdde0a4691f70
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-09 01:00:48 +0100

    posts page

--- graphql/schema.js
+++ graphql/schema.js
@@ -34,7 +34,7 @@ module.exports = buildSchema(`
     }
     type RootQuery {
         login(email: String!, password: String!): AuthData!
-        posts:PostData
+        posts(page:Int!):PostData
     }
     input PostInputData {
         title:String!
